326d7c137b6c9a072d2a1185c31c7e1f1c7b9cc6
nattawara <nattawara.jtk@gmail.com>
2024-05-09 13:22:49 +0700

    feat: declare variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstname = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurant = "Chef's Table by Chef James";
+const netPrice = 15000;
+const activeRestaurantCount = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netPrice = 5000;
+activeRestaurant = 2500;
